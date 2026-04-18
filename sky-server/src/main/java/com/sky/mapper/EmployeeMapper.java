2826a6e07290f50060c43cb153ff4aec43322e0c
keyl <2164282891@qq.com>
2026-04-18 16:51:07 +0800

    根据ID查询套餐 更新套餐 起售停售套餐 批量删除套餐功能代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -32,8 +32,17 @@ public interface EmployeeMapper {
      * @return
      */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+    /**
+     * 根据id修改员工信息
+     * @param employee
+     */
 @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
+/**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
 @Select("select * from employee where id = #{id}")
     Employee getById(Long id);
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,11 +1,16 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.anno.AutoFill;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Mapper
 public interface SetmealMapper {
@@ -19,4 +24,28 @@ public interface SetmealMapper {
     Integer countByCategoryId(Long id);
 @AutoFill(value = OperationType.INSERT)
     void insert(Setmeal setmeal);
+/**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+/**
+     * 根据id查询套餐数据
+     * @param id
+     * @return
+     */
+@Select("select * from setmeal where id = #{id}")
+    Setmeal getById(Long id);
+/**
+     * 批量删除套餐
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
+    /**
+     * 修改套餐
+     * @param setmeal
+     */
+@AutoFill(value = OperationType.UPDATE)
+    void update (Setmeal setmeal);
 }
